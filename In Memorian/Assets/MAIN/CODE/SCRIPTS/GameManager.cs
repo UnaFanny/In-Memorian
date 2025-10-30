7a7a40373c683a861dbf340ca943fb3561c43f46
Kobo <squevedo532@soyudemedellin.edu.co>
2025-10-30 11:33:01 -0500

    GameManager, Menu, Switch y Colisiones

---
+++ In Memorian/Assets/MAIN/CODE/SCRIPTS/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject menuPausa;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    public void Reintentar(string Derrota)
+    {
+        //puntos = 0;
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Juego");
+    }
+
+
+    private void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (menuPausa.activeSelf)
+                {
+                    EstadoDelJuego("Play");
+                    menuPausa.SetActive(false);
+                }
+                else
+                {
+                    EstadoDelJuego("Pause");
+                    menuPausa.SetActive(true);
+                }
+
+            }
+        
+    }
+
+    public void EstadoDelJuego(string estado)
+    {
+        switch (estado)
+        {
+
+            case "Play":
+                Time.timeScale = 1;
+                break;
+            case "Pause":
+                Time.timeScale = 0;
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+        }
+    }
+}
